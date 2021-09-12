1b32247c8c72b303b974ce72a7e80f81f4a5bfe4
barnord <bdarno92@gmail.com>
2021-09-11 18:31:51 -0700

    finished tests

--- dotnet/DataStructures/DataStructuresTests/BinaryTreeTests.cs
+++ dotnet/DataStructures/DataStructuresTests/BinaryTreeTests.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
 using DataStructures;
+using System.Collections.Generic;
 
 namespace DataStructuresTests
 {
@@ -39,10 +40,82 @@ namespace DataStructuresTests
     [Fact]
     public void DPreOrderTraversal()
     {
-      BinarySearchTree<int> tree = new();
-      tree.Add(tree.Root, 5);
+      BinaryTree<int> tree = new();
 
-      //List<int> list
+      Node<int> a = new Node<int>(5);
+      Node<int> b = new Node<int>(6);
+      Node<int> c = new Node<int>(7);
+      Node<int> d = new Node<int>(8);
+
+      tree.Root = a;
+      tree.Root.Left = b;
+      tree.Root.Right = c;
+      tree.Root.Left.Right = d;
+
+      List<int> list = new();
+      list.Add(5);
+      list.Add(6);
+      list.Add(8);
+      list.Add(7);
+
+      List<int> values = new();
+      tree.PreOrderTraversal(tree.Root, values);
+
+      Assert.Equal(list, values);
+    }
+
+    [Fact]
+    public void EInOrderTraversal()
+    {
+      BinaryTree<int> tree = new();
+
+      Node<int> a = new Node<int>(5);
+      Node<int> b = new Node<int>(6);
+      Node<int> c = new Node<int>(7);
+      Node<int> d = new Node<int>(8);
+
+      tree.Root = a;
+      tree.Root.Left = b;
+      tree.Root.Right = c;
+      tree.Root.Left.Right = d;
+
+      List<int> list = new();
+      list.Add(6);
+      list.Add(8);
+      list.Add(5);
+      list.Add(7);
+
+      List<int> values = new();
+      tree.InOrderTraversal(tree.Root, values);
+
+      Assert.Equal(list, values);
+    }
+
+    [Fact]
+    public void FPostOrderTraversal()
+    {
+      BinaryTree<int> tree = new();
+
+      Node<int> a = new Node<int>(5);
+      Node<int> b = new Node<int>(6);
+      Node<int> c = new Node<int>(7);
+      Node<int> d = new Node<int>(8);
+
+      tree.Root = a;
+      tree.Root.Left = b;
+      tree.Root.Right = c;
+      tree.Root.Left.Right = d;
+
+      List<int> list = new();
+      list.Add(8);
+      list.Add(6);
+      list.Add(7);
+      list.Add(5);
+
+      List<int> values = new();
+      tree.PostOrderTraversal(tree.Root, values);
+
+      Assert.Equal(list, values);
     }
   }
 }
